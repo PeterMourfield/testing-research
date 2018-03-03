5bd1f42489a3bb9b08733430640edd464cf817f3
pmourfield <peter.mourfield@gmail.com>
2018-03-03 17:35:18 -0500

    minor update

--- SampleUnitTests/AdderTests.cs
+++ SampleUnitTests/AdderTests.cs
@@ -8,12 +8,13 @@ namespace SampleUnitTests
         [TestMethod]
         public void One_Plus_One_Should_Equal_Two()
         {
+            // Arrange
             int a = 1;
             int b = 2;
             Adder adder = new Adder();
-
+            // Act
             int result = adder.Add(a, b);
-
+            // Assert
             Assert.ShouldEqual(2, result);
         }
     }
