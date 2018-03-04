d4a277950f70057c4e288a152c29e33dc86cc059
pmourfield <peter.mourfield@gmail.com>
2018-03-04 16:45:28 -0500

    Test Runner implementation

--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using UnitTestLibrary;
 
 namespace TestRunner
@@ -10,16 +12,35 @@ namespace TestRunner
     {
         static void Main(string[] args)
         {
-            Assembly testAssembly = null;
+            string path = string.Empty;
 
-            if(testAssembly != null)
+            if (args.Any())
+            {
+                path = args[0];
+                if (!File.Exists(path))
+                {
+                    path = Path.GetFullPath(path);
+                }
+            }
+
+            Assembly testAssembly = null;
+            if (String.IsNullOrEmpty(path))
+            {
+                Console.WriteLine(string.Format("Cannot find path {0}", path));
+            }
+            else
+            {
+                testAssembly = Assembly.LoadFile(path);
+            }
+
+            if (testAssembly != null)
             {
                 foreach (Type type in testAssembly.GetTypes())
                 {
                     if (type.GetCustomAttributes(typeof(TestClassAttribute), true).Length > 0)
                     {
                         var classToTest = Activator.CreateInstance(type);
-                        Console.WriteLine("Processing class " + type.Name);
+                        Console.WriteLine(string.Format("Processing class {0}", type.Name));
                         foreach(MethodInfo method in type.GetMethods())
                         {
                             if(method.GetCustomAttributes(typeof(TestMethodAttribute), false).Length > 0)
@@ -27,26 +48,28 @@ namespace TestRunner
                                 try
                                 {
                                     method.Invoke(classToTest, null);
-                                    Console.WriteLine("\t" + method.Name + "succeeded");
+                                    Console.WriteLine(string.Format("\tTest method {0} succeeded", method.Name));
                                 }
                                 catch(Exception ex)
                                 {
-                                    Console.WriteLine("\t" + method.Name + "failed");
-                                    if(ex.InnerException != null)
+                                    var sb = new StringBuilder();
+                                    sb.AppendFormat("\tTest method {0} failed", method.Name);
+                                    sb.AppendLine();
+                                    if (ex.InnerException != null)
                                     {
-                                        Console.WriteLine("\t\t" + ex.InnerException.Message);
+                                        sb.AppendFormat("\t\t{0}", ex.InnerException.Message);
                                     }
                                     else
                                     {
-                                        Console.WriteLine("\t\t" + ex.Message);
+                                        sb.AppendFormat("\t\t{0}", ex.Message);
                                     }
+                                    Console.WriteLine(sb.ToString());
                                 }
                             }
                         }
                     }
                 }
             }
-            Console.WriteLine();
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
         }
