2bb74ab2932bc5cf5a76e819c1a1835f2cec4389
pmourfield <peter.mourfield@gmail.com>
2018-03-04 09:40:51 -0500

    Added very simple IoC container

---
+++ ContainerLibrary/Container.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContainerLibrary
+{
+    public class Container
+    {
+        public delegate object InstanceCreator(Container container);
+
+        Dictionary<Type, InstanceCreator> registeredTypes = new Dictionary<Type, InstanceCreator>();
+
+        public void Register(Type typeToRegister, InstanceCreator creatorDelegate)
+        {
+            registeredTypes.Add(typeToRegister, creatorDelegate);
+        }
+
+        public void Register<T>(InstanceCreator creatorDelegate)
+        {
+            Type registeredType = typeof(T);
+            registeredTypes.Add(registeredType, creatorDelegate);
+        }
+
+        public object GetInstance(Type typeToGet)
+        {
+            return registeredTypes[typeToGet](this);
+        }
+
+        public T GetInstance<T>()
+        {
+            Type registeredType = typeof(T);
+            return (T)registeredTypes[registeredType](this);
+        }
+    }
+}
