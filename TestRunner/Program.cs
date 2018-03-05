135a8673e0877e4a53887059cdadf72b1bafd234
pmourfield <peter.mourfield@gmail.com>
2018-03-05 14:02:33 -0500

    a few formatting updates

--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -30,45 +29,47 @@ namespace TestRunner
             }
             else
             {
+                Console.WriteLine(string.Format("Loading {0}", path));
                 testAssembly = Assembly.LoadFrom(path);
             }
 
             if (testAssembly != null)
             {
+                Console.WriteLine("Starting execution");
                 foreach (Type type in testAssembly.GetTypes())
                 {
                     if (type.GetCustomAttributes(typeof(TestClassAttribute), true).Length > 0)
                     {
                         var classToTest = Activator.CreateInstance(type);
-                        Console.WriteLine(string.Format("Processing class {0}", type.Name));
                         foreach(MethodInfo method in type.GetMethods())
                         {
-                            if(method.GetCustomAttribute(typeof(TestMethodAttribute)) != null)
+                            var testMethodAttribute = method.GetCustomAttribute(typeof(TestMethodAttribute));
+                            if (testMethodAttribute != null)
                             {
                                 try
                                 {
                                     method.Invoke(classToTest, null);
-                                    Console.WriteLine(string.Format("\tSUCCEEDED: Test method {0}", method.Name));
+                                    Console.WriteLine(string.Format("SUCCEEDED\t{0}.{1}", type.FullName, method.Name));
                                 }
                                 catch(Exception ex)
                                 {
                                     var expectedExceptionAttribute = method.GetCustomAttribute(typeof(ExpectedExceptionAttribute));
                                     if (expectedExceptionAttribute != null && ex.InnerException.GetType() == ((ExpectedExceptionAttribute)expectedExceptionAttribute).ExpectedType)
                                     {
-                                        Console.WriteLine(string.Format("\tSUCCEEDED: Test method {0}", method.Name));
+                                        Console.WriteLine(string.Format("SUCCEEDED\t{0}.{1}", type.FullName, method.Name));
                                     }
                                     else
                                     {
                                         var sb = new StringBuilder();
-                                        sb.AppendFormat("\tFAILED: Test method {0}", method.Name);
+                                        sb.AppendFormat("FAILED\t\t{0}.{1}", type.FullName, method.Name);
                                         sb.AppendLine();
                                         if (ex.InnerException != null)
                                         {
-                                            sb.AppendFormat("\t\t{0}", ex.InnerException.Message);
+                                            sb.AppendFormat("\t\t\t***{0}", ex.InnerException.Message);
                                         }
                                         else
                                         {
-                                            sb.AppendFormat("\t\t{0}", ex.Message);
+                                            sb.AppendFormat("\t\t\t***{0}", ex.Message);
                                         }
                                         Console.WriteLine(sb.ToString());
                                     }
